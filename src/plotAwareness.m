76ff30e5dadfee398bc3c4f0367ef2838041a00d
davidwilby <d.wilby@sheffield.ac.uk>
2023-04-27 14:21:17 +0100

    group by experience instead of role

--- src/plotAwareness.m
+++ src/plotAwareness.m
@@ -7,38 +7,31 @@ function plotAwareness(awarenessTable, awarenessColumn)
     useOutsideMatlabButNotWith = awarenessTable(contains(awarenessTable{:,awarenessColumn}, "Aware of and use outside MATLAB") & ~contains(awarenessTable{:,awarenessColumn},"Aware of and use with MATLAB"), :);
 
     figure;
-%     barVals = [height(notAware) height(awareNoUse) height(useWithMatlab) height(useOutsideMatlab) height(useOutsideMatlabButNotWith)];
     
-    barVals = [countByRole(notAware, "Undergraduate student") countByRole(notAware, "Doctoral student") countByRole(notAware, "Postdoctoral Researcher / Research Fellow") countByRole(notAware, "Research Software Engineer") countByRole(notAware, "Academic");...
-        countByRole(awareNoUse, "Undergraduate student") countByRole(awareNoUse, "Doctoral student") countByRole(awareNoUse, "Postdoctoral Researcher / Research Fellow") countByRole(awareNoUse, "Research Software Engineer") countByRole(awareNoUse, "Academic");...
-        countByRole(useWithMatlab, "Undergraduate student") countByRole(useWithMatlab, "Doctoral student") countByRole(useWithMatlab, "Postdoctoral Researcher / Research Fellow") countByRole(useWithMatlab, "Research Software Engineer") countByRole(useWithMatlab, "Academic");...
-        countByRole(useOutsideMatlab, "Undergraduate student") countByRole(useOutsideMatlab, "Doctoral student") countByRole(useOutsideMatlab, "Postdoctoral Researcher / Research Fellow") countByRole(useOutsideMatlab, "Research Software Engineer") countByRole(useOutsideMatlab, "Academic");...
-        countByRole(useOutsideMatlabButNotWith, "Undergraduate student") countByRole(useOutsideMatlabButNotWith, "Doctoral student") countByRole(useOutsideMatlabButNotWith, "Postdoctoral Researcher / Research Fellow") countByRole(useOutsideMatlabButNotWith, "Research Software Engineer") countByRole(useOutsideMatlabButNotWith, "Academic");...
+    barVals = [countByExperience(notAware, "Novice") countByExperience(notAware, "Competent Practitioner") countByExperience(notAware, "Expert");...
+        countByExperience(awareNoUse , "Novice") countByExperience(awareNoUse , "Competent Practitioner") countByExperience(awareNoUse , "Expert");...
+        countByExperience(useWithMatlab, "Novice") countByExperience(useWithMatlab, "Competent Practitioner") countByExperience(useWithMatlab, "Expert");...
+        countByExperience(useOutsideMatlab, "Novice") countByExperience(useOutsideMatlab, "Competent Practitioner") countByExperience(useOutsideMatlab, "Expert");...
+        countByExperience(useOutsideMatlabButNotWith, "Novice") countByExperience(useOutsideMatlabButNotWith, "Competent Practitioner") countByExperience(useOutsideMatlabButNotWith, "Expert");...
         ];
     
     y = 100*barVals/height(awarenessTable);
 
-    labels = {'Not aware of', 'Aware of but don''t use', 'Use with MATLAB', 'Use outside MATLAB', 'Use Outside but not with MATLAB'};
+    labels = {'Not aware of', 'Aware of but don''t use', 'Use with MATLAB', 'Use outside MATLAB', 'Use outside but not with MATLAB'};
     x = categorical(labels);
     x = reordercats(x,labels);
 
     handle = bar(x, y, 'stacked');
     ylabel(strcat("% of ", num2str(height(awarenessTable)), " respondents"))
 
-    stackedBarLabels = {'Undergraduate Students', 'Doctoral Students', 'Postdoctoral Researchers/Research Fellows', 'RSEs/Developers','Academics'};
+    stackedBarLabels = {'Novice', 'Competent Practitioner', 'Expert'};
     legend(flip(handle), flip(stackedBarLabels), "Location","northeastoutside")
 
     set(gcf,'Units','normalized','Position',[0 0 1 1.5]);
     colormap parula
 
-%     xtips1 = b(1).XEndPoints;
-%     ytips1 = b(1).YEndPoints;
-%     labels1 = string(barVals);
-%     text(xtips1,ytips1,labels1,'HorizontalAlignment','center',...
-%         'VerticalAlignment','bottom')
-
-    function count = countByRole(dataTable, role)
-        count = height(dataTable(contains(dataTable.role, role), :));
+    function count = countByExperience(dataTable, value)
+        count = height(dataTable(contains(dataTable.matlabExperience, value), :));
     end
 
 end
